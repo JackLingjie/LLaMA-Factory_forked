4b6d3fa23af2027cbd78b295bb8fd58e0792cd02
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2024-12-27 03:46:42 +0000

    add job

--- bash_script/run_llama.sh
+++ bash_script/run_llama.sh
@@ -24,3 +24,5 @@ llamafactory-cli train bash_script/qwen2vl_preprocess_val.yaml
 bash_script/qwen2vl_lora_sft_preprocess.yaml
 
 FORCE_TORCHRUN=1 llamafactory-cli train bash_script/qwen2vl_lora_sft_tokenized_job.yaml
+
+FORCE_TORCHRUN=1 llamafactory-cli train bash_script/qwen2vl_lora_sft_tokenized_job_zero.yaml
