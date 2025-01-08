9e12ab3a8c81340827a8aadfddfff8e767be7038
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-01-08 09:09:48 +0000

    tran llm and merger

--- src/llamafactory/model/model_utils/visual.py
+++ src/llamafactory/model/model_utils/visual.py
@@ -141,7 +141,8 @@ def get_forbidden_modules(config: "PretrainedConfig", finetuning_args: "Finetuni
         if finetuning_args.train_mm_proj_only:
             forbidden_modules.update({"visual.patch_embed", "visual.blocks", "model", "lm_head"})
         elif finetuning_args.freeze_vision_tower:
-            forbidden_modules.add("visual")
+            forbidden_modules.add({"visual.patch_embed", "visual.blocks"})
+            # forbidden_modules.add("visual")
 
     return forbidden_modules
 
