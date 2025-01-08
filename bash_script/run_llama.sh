ba7dd6b832309f86d1644c4046d4d04d9953e1b2
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-01-08 06:33:39 +0000

    all test

--- bash_script/run_llama.sh
+++ bash_script/run_llama.sh
@@ -38,3 +38,8 @@ llamafactory-cli export bash_script/merge/qwen2vl_sft_mmcode_merge.yaml
 llamafactory-cli train bash_script/qwen2vl_coder_lora_sft_test.yaml
 
 llamafactory-cli train bash_script/qwen2vl_coder_pretrain_sft_test_job_preprocess.yaml
+
+FORCE_TORCHRUN=1 llamafactory-cli train bash_script/qwen2vl_coder_pretrain_sft_test_job.yaml
+
+
+llamafactory-cli train bash_script/qwen2vl_coder_lora_sft_test_all.yaml
