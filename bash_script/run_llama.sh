c798e874c5c33d6b82c3d5d2f66d2bc0533e4551
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-01-08 09:02:40 +0000

    zero0

--- bash_script/run_llama.sh
+++ bash_script/run_llama.sh
@@ -49,5 +49,14 @@ FORCE_TORCHRUN=1 llamafactory-cli train bash_script/qwen2vl_coder_lora_sft_test_
 FORCE_TORCHRUN=1 llamafactory-cli train bash_script/qwen2vl_coder_all_part_test.yaml
 llamafactory-cli train bash_script/qwen2vl_coder_pretrain_sft_test_job_preprocess.yaml
 
+export OMP_NUM_THREADS=8
+export NCCL_IB_DISABLE=0
+export NCCL_IB_GID_INDEX=3
+export NCCL_SOCKET_IFNAME=eth0
+export NCCL_DEBUG=INFO
+
+
+export NCCL_NET=IB
+
 FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/qwen2vl_coder_pretrain_sft_test_job_2nodes.yaml
 FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/qwen2vl_coder_pretrain_sft_test_job_2nodes.yaml
