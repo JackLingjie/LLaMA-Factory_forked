3d13bcd1e35db23b5fc72dbd3004fbe6f60c0604
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-01-08 07:22:59 +0000

    run test

--- bash_script/run_qwen.sh
+++ bash_script/run_qwen.sh
@@ -3,3 +3,5 @@ FORCE_TORCHRUN=1 llamafactory-cli train bash_script/qwen2vl_lora_sft_mmcode_2500
 FORCE_TORCHRUN=1 llamafactory-cli train bash_script/qwen2vl_lora_sft_mmcode_2200_2epoch_job.yaml
 
 FORCE_TORCHRUN=1 llamafactory-cli train bash_script/qwen2vl_lora_sft_mmcode_2500_gpt4o_job.yaml
+
+FORCE_TORCHRUN=1 llamafactory-cli train bash_script/qwen2vl_coder_llm_merger_test.yaml
