d5b222ae5a770e9ed9cbde0f2b3679fc178fbd7a
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-01-09 15:12:36 +0000

    exp

--- bash_script/run_llama.sh
+++ bash_script/run_llama.sh
@@ -21,7 +21,7 @@ llamafactory-cli train bash_script/qwen2vl_lora_sft_job_preprocessing.yaml
 
 llamafactory-cli train bash_script/qwen2vl_preprocess_val.yaml
 
-bash_script/qwen2vl_lora_sft_preprocess.yaml
+llamafactory-cli train bash_script/qwen2vl_lora_sft_amd_test.yaml
 
 FORCE_TORCHRUN=1 llamafactory-cli train bash_script/qwen2vl_lora_sft_tokenized_job.yaml
 
