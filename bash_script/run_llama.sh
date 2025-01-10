50967f241ac73bb88185f04cffe0f0d821b6f344
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-01-10 15:21:09 +0000

    web2code exp

--- bash_script/run_llama.sh
+++ bash_script/run_llama.sh
@@ -84,3 +84,10 @@ FORCE_TORCHRUN=1 llamafactory-cli train bash_script/qwen2vl_coder_pretrain_sft_t
 
 FORCE_TORCHRUN=1 llamafactory-cli train bash_script/qwen2vl_coder_pretrain_sft_test_job_2nodes_1e5_web2code.yaml
 llamafactory-cli train bash_script/qwen2vl_coder_pretrain_sft_test_job_2nodes_1e5_web2code_debug.yaml
+
+llamafactory-cli train bash_script/qwen2vl_coder_mid_sft_mm_proj_test_job_2nodes_1e3_preprocess.yaml
+
+export NCCL_NET=IB
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/qwen2vl_coder_pretrain_mid_sft_llm_job_2nodes_1e5_web2code.yaml
+export NCCL_NET=IB
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/qwen2vl_coder_pretrain_mid_sft_llm_job_2nodes_1e5_web2code.yaml
