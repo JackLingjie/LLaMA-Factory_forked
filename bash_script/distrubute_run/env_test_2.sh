0706c5d3f27359277de3b191a3a7e00ba7e60f9f
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-01-11 16:08:58 +0000

    pdsh test

--- bash_script/distrubute_run/env_test_2.sh
+++ bash_script/distrubute_run/env_test_2.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-
-pdsh -R ssh -w ^bash_script/distrubute_run/node_config/nodes.txt \
-'bash -c "cd /tmp && \
-git clone https://github.com/JackLingjie/LLaMA-Factory_forked.git && \
-cd LLaMA-Factory_forked"'
+pdsh -R ssh -w ^bash_script/distrubute_run/node_config/nodes.txt 'bash -c "cd /tmp && git clone https://github.com/JackLingjie/LLaMA-Factory_forked.git && cd LLaMA-Factory_forked"'
