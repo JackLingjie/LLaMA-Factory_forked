96fc3ff48fd87d6fcfa8b59e5f13f7b9470ca4e3
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-01-12 12:37:51 +0000

    decrease num proc

--- bash_script/run_qwen.sh
+++ bash_script/run_qwen.sh
@@ -12,3 +12,9 @@ llamafactory-cli train bash_script/qwen2vl_coder_mid_sft_test_job_preprocess_1M_
 
 llamafactory-cli train bash_script/qwen2vl_coder_mid_sft_mm_proj_test_job_2nodes_1e3_preprocess_test.yaml
 
+unset NCCL_NET; unset FORCE_TORCHRUN; unset NNODES; unset NODE_RANK; unset MASTER_ADDR; unset MASTER_PORT; llamafactory-cli train bash_script/pretrain_ocr_code_v2_mm_proj_job_2nodes_1e3_preprocess.yaml
+llamafactory-cli train bash_script/pretrain_ocr_code_v2_mm_proj_job_2nodes_1e3_preprocess.yaml
+
+unset NCCL_NET; unset FORCE_TORCHRUN; unset NNODES; unset NODE_RANK; unset MASTER_ADDR; unset MASTER_PORT; llamafactory-cli train bash_script/pretrain_ocr_code_v3_mm_proj_job_2nodes_1e3_preprocess.yaml
+
+unset NCCL_NET; unset FORCE_TORCHRUN; unset NNODES; unset NODE_RANK; unset MASTER_ADDR; unset MASTER_PORT; llamafactory-cli train bash_script/pretrain_ocr_code_v4_mm_proj_job_2nodes_1e3_preprocess.yaml
