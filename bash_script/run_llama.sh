b2b66ab70af4700309cfd6a4814028cae8ad607c
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-01-14 03:39:04 +0000

    re process

--- bash_script/run_llama.sh
+++ bash_script/run_llama.sh
@@ -151,3 +151,5 @@ export NCCL_NET=IB
 FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-2 MASTER_PORT=12335 llamafactory-cli train bash_script/pretrain_code_111k_mm_proj_job_2nodes_1e3.yaml
 export NCCL_NET=IB
 FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-2 MASTER_PORT=12335 llamafactory-cli train bash_script/pretrain_code_111k_mm_proj_job_2nodes_1e3.yaml
+
+llamafactory-cli train bash_script/pretrain_web2code_670k_mm_proj_job_2nodes_1e3_preprocess.yaml
