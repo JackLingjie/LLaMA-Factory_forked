fac9ca58794c14c5f54a2464d79194838de32241
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-01-23 08:22:29 +0000

    evol chart exp

--- bash_script/run_llama.sh
+++ bash_script/run_llama.sh
@@ -256,3 +256,9 @@ FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-2 MASTER_PORT=12335 llama
 FORCE_TORCHRUN=1 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_code_data_mix_20k_purify.yaml
 
 FORCE_TORCHRUN=1 llamafactory-cli train bash_script/qwen2vl_text_img_exp.yaml
+
+export NCCL_NET=IB
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_chart_evol_40k.yaml
+export NCCL_NET=IB
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_chart_evol_40k.yaml
+
