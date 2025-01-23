32b95ab6074d846f06c9d41d0a133e2dda8ec6c6
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-01-23 06:58:03 +0000

    debuging

--- run_gpu.py
+++ run_gpu.py
@@ -7,7 +7,7 @@ import sys
  
 def parse_args():
     parser = argparse.ArgumentParser(description='Matrix multiplication')
-    parser.add_argument('--gpus', help='gpu amount', default=1, type=int)
+    parser.add_argument('--gpus', help='gpu amount', default=8, type=int)
     parser.add_argument('--size', help='matrix size', default=12000, type=int)
     parser.add_argument('--interval', help='sleep interval', default=0.01, type=float)
     args = parser.parse_args()
@@ -30,6 +30,6 @@ def matrix_multiplication(args):
         time.sleep(args.interval)
  
 if __name__ == "__main__":
-    # usage: python run_gpu.py --size 20000 --gpus 4 --interval 0.01
+    # usage: python run_gpu.py --size 20000 --gpus 8 --interval 0.01
     args = parse_args()
     matrix_multiplication(args)
