4c8ea3acc7ccd41fc1cb95def58d973a60730d5d
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-01-23 06:43:37 +0000

    new model exp

--- bash_script/run_llama.sh
+++ bash_script/run_llama.sh
@@ -231,3 +231,24 @@ FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llama
 export NCCL_NET=IB
 FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_chart_exp.yaml
 
+
+export NCCL_NET=IB
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_web2code_rick_revised_750k_v1.yaml
+export NCCL_NET=IB
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_web2code_rick_revised_750k_v1.yaml
+
+export NCCL_NET=IB
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-2 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_web2code_rick_revised_html_tag_750k_v2.yaml
+export NCCL_NET=IB
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-2 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_web2code_rick_revised_html_tag_750k_v2.yaml
+
+
+export NCCL_NET=IB
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_code_data_mix_21k.yaml
+export NCCL_NET=IB
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_code_data_mix_21k.yaml
+
+export NCCL_NET=IB
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_code21_html80_mix_101k.yaml
+export NCCL_NET=IB
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_code21_html80_mix_101k.yaml
