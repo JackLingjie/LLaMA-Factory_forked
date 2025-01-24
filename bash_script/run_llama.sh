6be29a728c3da6b8d458161b5b5a1438e88c4e27
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-01-24 07:33:53 +0000

    code test

--- bash_script/run_llama.sh
+++ bash_script/run_llama.sh
@@ -284,6 +284,10 @@ FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llama
 export NCCL_NET=IB
 FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_chart_cot_39k.yaml
 
+export NCCL_NET=IB
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_html_taco_revised_mix_95k.yaml
+export NCCL_NET=IB
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_html_taco_revised_mix_95k.yaml
 
 
 bash_script/stage2_llm_2nodes_1e5_chart_cot_39k.yaml
