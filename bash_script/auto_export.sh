88b311f080d60bc9447b609f77be252c3a1d7b23
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-01-24 05:00:58 +0000

    cot exp

--- bash_script/auto_export.sh
+++ bash_script/auto_export.sh
@@ -1,2 +1,2 @@
-llamafactory-cli export bash_script/merge/stage2_llm_2nodes_1e5_web2code_bsz128_1e5_chart_evol_40k_282.yaml
+llamafactory-cli export bash_script/merge/stage2_llm_2nodes_1e5_web2code_bsz128_1e5_code_revised_evol_50k_355.yaml
 # llamafactory-cli export bash_script/merge/stage2_llm_stage2_llm_2nodes_1e5_web2code_bsz128_1e5_code20_html80_mix_100k_705.yaml
--- bash_script/run_llama.sh
+++ bash_script/run_llama.sh
@@ -266,3 +266,24 @@ export NCCL_NET=IB
 FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_code_revised_evol_50k.yaml
 export NCCL_NET=IB
 FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_code_revised_evol_50k.yaml
+
+
+
+export NCCL_NET=IB
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_html_chart_mix_v1_119k.yaml
+export NCCL_NET=IB
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_html_chart_mix_v1_119k.yaml
+
+export NCCL_NET=IB
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-2 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_html_chart_mix_v2_130k.yaml
+export NCCL_NET=IB
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-2 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_html_chart_mix_v2_130k.yaml
+
+export NCCL_NET=IB
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_chart_cot_39k.yaml
+export NCCL_NET=IB
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_chart_cot_39k.yaml
+
+
+
+bash_script/stage2_llm_2nodes_1e5_chart_cot_39k.yaml
