f28d80613ea4dd6ed7ef7b9a014f0184b3dc3f56
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-02-03 09:39:14 +0000

    auto run

---
+++ bash_script/auto_run/run2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+
+# export NCCL_NET=IB
+# FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_chartbench_images_46k_raw_answer.yaml
+export NCCL_NET=IB
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/dpo_chart_qwen72b_47k_job.yaml
+
+
+python run_gpu.py
