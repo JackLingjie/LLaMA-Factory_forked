a976d231a5a102b65da50d52d92b4f4ea782d671
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-02-05 12:39:29 +0000

    sft exp

--- bash_script/auto_run/run1.sh
+++ bash_script/auto_run/run1.sh
@@ -8,7 +8,7 @@ set -x
 # export NCCL_NET=IB
 # FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_chartbench_images_46k_raw_answer.yaml
 export NCCL_NET=IB
-FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/dpo_html_chart_mix_v3_mimic_image_7b_27k.yaml
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_html_chart_code_data_v1_raw_190k.yaml
 
 
 
--- bash_script/auto_run/run4.sh
+++ bash_script/auto_run/run4.sh
@@ -7,7 +7,7 @@ set -x
 # FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/dpo_chart_qwen72b_47k_job.yaml
 
 export NCCL_NET=IB
-FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-2 MASTER_PORT=12335 llamafactory-cli train bash_script/dpo_html_chart_mix_v3_origin_image_7b_27k.yaml
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-2 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_html_chart_code_data_v2_code_190k.yaml
 
 
 python run_gpu.py
