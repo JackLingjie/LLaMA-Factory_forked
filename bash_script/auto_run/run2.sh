666a5409e1d0de9b6a1d32f6e9d1782cc492526b
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-02-05 10:40:25 +0000

    html dpo

--- bash_script/auto_run/run2.sh
+++ bash_script/auto_run/run2.sh
@@ -10,7 +10,7 @@ set -x
 # FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_html_chartbench_mix_v3_127k.yaml
 
 export NCCL_NET=IB
-FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/dpo_html_chart_mix_v3_chart_v1_origin_img_24k.yaml
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=1 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/dpo_html_chart_mix_v3_mimic_image_7b_27k.yaml
 
 
 python run_gpu.py
