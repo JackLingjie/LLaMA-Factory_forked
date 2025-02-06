b0927adfd49a7b6ca21026e2030009dd99c1bb60
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-02-06 17:05:24 +0000

    code dpo exp

--- bash_script/auto_run/run1.sh
+++ bash_script/auto_run/run1.sh
@@ -25,7 +25,11 @@ set -x
 # export NCCL_NET=IB
 # FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/dpo_chart_html_origin_41k_72b.yaml
 
+# export NCCL_NET=IB
+# FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/dpo_html_chart_mix_chart_html_code_4o_95k.yaml
+
 export NCCL_NET=IB
-FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/dpo_html_chart_mix_chart_html_code_4o_95k.yaml
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/dpo_html_chart_mix_v3_4o_code_score80_42k.yaml
+
 
 python run_gpu.py
