00a8df105cca0b139d283656491b72611bf00e25
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-02-07 08:11:02 +0000

    stage exp2

--- bash_script/auto_run/run1.sh
+++ bash_script/auto_run/run1.sh
@@ -40,9 +40,11 @@ set -x
 # export NCCL_NET=IB
 # FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/dpo_mix190k_dpo_code37k.yaml
 
+# export NCCL_NET=IB
+# FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_qwen2_text2.5_1M_html_chart_code_data_v2_code_190k.yaml
+
 export NCCL_NET=IB
-FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_qwen2_text2.5_1M_html_chart_code_data_v2_code_190k.yaml
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_qwencoder2.5_500k_html_chart_code_data_v2_code_190k.yaml
 
 
-# bash_script/stage2_qwencoder2.5_500k_html_chart_code_data_v2_code_190k.yaml
 python run_gpu.py
