6824af6d5886f6a878f322a4765d14af4699718c
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-02-07 08:44:20 +0000

    data filter exp

--- bash_script/auto_run/run1.sh
+++ bash_script/auto_run/run1.sh
@@ -43,8 +43,10 @@ set -x
 # export NCCL_NET=IB
 # FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_qwen2_text2.5_1M_html_chart_code_data_v2_code_190k.yaml
 
-export NCCL_NET=IB
-FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_qwencoder2.5_500k_html_chart_code_data_v2_code_190k.yaml
+# export NCCL_NET=IB
+# FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_qwencoder2.5_500k_html_chart_code_data_v2_code_190k.yaml
 
+export NCCL_NET=IB
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_html_chart_stack_data_170k.yaml
 
 python run_gpu.py
