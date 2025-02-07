65f3fe416cb487b3c13d7d41330c4ffce64f8730
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-02-07 08:52:43 +0000

    exp

--- bash_script/auto_run/run3.sh
+++ bash_script/auto_run/run3.sh
@@ -32,11 +32,11 @@ set -x
 # FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-2 MASTER_PORT=12335 llamafactory-cli train bash_script/dpo_mix190k_html_chart_code37_origin_89k.yaml
 
 
-export NCCL_NET=IB
-FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-2 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_qwen2_text2.5_500k_html_chart_code_data_v2_code_190k.yaml
-
 # export NCCL_NET=IB
-# FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-2 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_html_code_chart_stack_256k.yaml
+# FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-2 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_qwen2_text2.5_500k_html_chart_code_data_v2_code_190k.yaml
+
+export NCCL_NET=IB
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-2 MASTER_PORT=12335 llamafactory-cli train bash_script/stage2_llm_2nodes_1e5_html_code_chart_stack_256k.yaml
 
 
 python run_gpu.py
