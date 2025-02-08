0de2b4f4c7f711cdc5b5e61d7d147bbf0f7e90c6
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-02-08 17:37:29 +0000

    v3

--- bash_script/auto_run_4/run3.sh
+++ bash_script/auto_run_4/run3.sh
@@ -15,7 +15,8 @@ yaml_files=(
     # "bash_script/dpo_qwen2vl_v1_code_190k_html_chart_code37_origin_89k.yaml"
     # "bash_script/stage2_llm_2nodes_1e5_html_chart_stack_data_170k.yaml"
     # "bash_script/dpo_qwen2vl_v1_code_190k_chart_html_origin_95k.yaml"
-    "bash_script/qwen72b_html_chart_code_data_v2_code_190k.yaml"
+    # "bash_script/qwen72b_html_chart_code_data_v2_code_190k.yaml"
+    "bash_script/qwen72b_html_chartbench_mix_v3_127k.yaml"
 )
 
 YAML_FILE="${yaml_files[0]}"
