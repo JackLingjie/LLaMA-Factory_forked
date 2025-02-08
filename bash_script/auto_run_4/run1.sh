afaa5a6042a3f1a795c39bc586ebb881375ea3f3
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-02-08 17:17:45 +0000

    4nodes

--- bash_script/auto_run_4/run1.sh
+++ bash_script/auto_run_4/run1.sh
@@ -26,6 +26,6 @@ yaml_files=(
 YAML_FILE="${yaml_files[0]}"
 
 export NCCL_NET=IB
-FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train "$YAML_FILE"
+FORCE_TORCHRUN=1 NNODES=4 NODE_RANK=0 MASTER_ADDR=node-0 MASTER_PORT=12335 llamafactory-cli train "$YAML_FILE"
 
 python run_gpu.py
